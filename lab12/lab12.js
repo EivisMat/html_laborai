a9165fbd8d49762e3e125a58ff7de7d4aa3738a0
EivisMat <149148274+EivisMat@users.noreply.github.com>
2023-12-08 11:12:26 +0200

    Update lab12.js

--- lab12/lab12.js
+++ lab12/lab12.js
@@ -18,35 +18,58 @@ function handleFormData(){
     } else {
         const regex = /^\d+$/;
         if(regex.test(Student.userCode) == false) {
-            alert("Studento pažymėjimo kodas turi neleidžiamus simbolius ir/arba raides! (Galimi tik skaičiai)")
+            alert("Studento pažymėjimo kodas turi neleidžiamus simbolius ir/arba raides! (Galimi tik skaičiai)");
             return;
         }
     }
 
-    // Patikra buvo teisinga, pritaikome spalvas:
+    // Check if marks are valid (cannot be negative or above 10)
+    for(let i=0;i<5;i++){
+        if (Student.userMarks[i] < 0) {
+            alert("Pažymys negali būti neigiamas!");
+            return;
+        } else if(Student.userMarks[i] > 10) {
+            alert("Pažymys negali būti daugiau negu 10!");
+            return;
+        }
+    }
 
-    // Pirmas variantas
+    // Checks passed, set marks color:
+
+    // First option - set color by lowest mark
     // const min = Math.min(...Student.userMarks);
     // if (min <= 4) {
     //     document.getElementById("marks-display").style.color = "red";
-    // } else if(min > 4 && min <= 8) {
+    // } else if(min <= 8) {
     //     document.getElementById("marks-display").style.color = "orange";
     // } else {
-    //     document.getElementById("marks-display").style.color = "lgreen";
+    //     document.getElementById("marks-display").style.color = "limegreen";
     // }
 
-    // Antras variantas
-    for(let i=0; i<5; i++){
-        val = parseInt(marks[i].value);
-        if (val <= 4) {
-            marks[i].style.color = "red";
-        } else if(val > 4 && val <= 8) {
-            marks[i].style.color = "orange";
-        } else {
-            marks[i].style.color = "limegreen";
-        }
-    }      
+    // Second option - set each mark's colour individually
+    // for(let i=0; i<5; i++){
+    //     val = parseInt(marks[i].value);
+    //     if (val <= 4) {
+    //         marks[i].style.color = "red";
+    //     } else if(val <= 8) {
+    //         marks[i].style.color = "orange";
+    //     } else {
+    //         marks[i].style.color = "limegreen";
+    //     }
+    // }      
 
+    // Third option - set color by average
+    let sum = 0;
+    Student.userMarks.forEach((num) => sum+=num);
+    const average = sum/Student.userMarks.length;
+    console.log(average);
+    if (average <= 4) {
+        document.getElementById("marks-display").style.color="red";
+    } else if(average <= 8) {
+        document.getElementById("marks-display").style.color="orange";
+    } else {
+        document.getElementById("marks-display").style.color="limegreen";
+    }
 
     // Display
     console.log(Student);
